b8471a160da5c1a114d7ae3d6f209f94f373feb6
ruwan <rwnranaweera@gmail.com>
2021-02-07 05:06:35 +0530

    pra python

---
+++ 1.py
@@ -0,0 +1,11 @@
+a =-5
+if a:
+    print(a)
+else:
+    print("false")
+
+b=20
+if b>10:
+    print('b is greater than 10')
+else:
+    print("b is not greater than 10")
---
+++ 2.py
@@ -0,0 +1,9 @@
+a=0
+while a<5:
+    print(a)
+    a = a+1
+else:
+    print("a value is greater than or equal to 5")
+
+print(a)
+    
